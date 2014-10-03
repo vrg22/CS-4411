dfe58cac2d0db706dfcd07f57b860769ffa49f07
Vivek Gaddam <vrg22@cornell.edu>
2014-10-03 12:57:11 -0400

    Multi-queue works now, need to implement scheduler

--- release/multilevel_queue.c
+++ release/multilevel_queue.c
@@ -10,16 +10,14 @@
  */
 multilevel_queue_t multilevel_queue_new(int number_of_levels) {
 	multilevel_queue_t ml_queue;
-	//queue_t lvls[number_of_levels];			//queue_t q;
 	int i;
 
 	ml_queue = (multilevel_queue_t) malloc(sizeof(struct multilevel_queue));	// Malloc the multilevel queue
 	if (ml_queue == NULL) return NULL;	// malloc failure
 
 	ml_queue->num_levels = number_of_levels;
-	ml_queue->levels = malloc(sizeof(queue_t) * number_of_levels);
-	/*&lvls[0]*/;		//queue_t array declaration
-										//queue_t (ml_queue->levels)[ml_queue->num_levels];
+	ml_queue->levels = malloc(sizeof(queue_t) * number_of_levels);		//queue_t array declaration
+
 	//Create each level's queue
 	for (i = 0; i < number_of_levels; i++) {
 		(ml_queue->levels)[i] = queue_new();
@@ -27,14 +25,7 @@ multilevel_queue_t multilevel_queue_new(int number_of_levels) {
 			printf("malloc failed!\n");
 			return NULL;	// malloc failure
 		}
-		printf("level: %i, len: %i\n", i, queue_length((ml_queue->levels)[i]));
-
-		// if (i == 0){
-		// 	q = (ml_queue->levels)[i];
-		// 	printf("Ptr of the actual queue: %p\n", q);
-		// }
 	}
-	printf("Levels Queue Ptr: %p\n\n", ml_queue->levels);
 	return ml_queue;
 }
 
@@ -43,24 +34,11 @@ multilevel_queue_t multilevel_queue_new(int number_of_levels) {
  * Appends an void* to the multilevel queue at the specified level. Return 0 (success) or -1 (failure).
  */
 int multilevel_queue_enqueue(multilevel_queue_t queue, int level, void* item) {
-	queue_t q; int rslt;
-	// int i = 6;
+	queue_t q; 
+	int rslt;
 
 	q = (queue->levels)[level];
-	printf("Levels Queue Ptr: %p\n", queue->levels);
-	// printf("Item Queue Ptr: %p\n", item);
-	printf("Ptr of the actual queue: %p\n", q);
-	
-	printf("level: %i, len: %i\n", level, queue_length((queue->levels)[level]));
-	// if (q != NULL){
-	// 	printf("level: %i, len: %i\n", level, queue_length(q));
-	// }
-	
 	rslt = queue_append(q, item);
-	printf("level: %i, len: %i\n", level, queue_length((queue->levels)[level]));
-
-	q = (queue->levels)[level];
-	printf("Ptr of the actual queue: %p\n", q);
 
 	return rslt;
 }
@@ -73,31 +51,18 @@ int multilevel_queue_enqueue(multilevel_queue_t queue, int level, void* item) {
  */
 int multilevel_queue_dequeue(multilevel_queue_t queue, int level, void** item) {
 	queue_t q;
-	// int rslt;
 	int i, n;
 
-	// rslt = queue_dequeue(q, item);
-	// printf("Got here\n");
-	// return rslt;
-
-	// printf("in dequeue\n");
 	n = queue->num_levels;
 	i = 0;
 
 	while (i < n){	//check only n levels
-		printf("Levels Queue Ptr: %p\n", queue->levels);
-		printf("Level: %i\n", level);
 		q = (queue->levels)[level];
-		// printf("Ptr of the actual queue: %p\n", q);
 
 		if (queue_dequeue(q, item) == 0){
-			printf("Successful dequeue\n");
 			return level;		// Success!
 		}
-		else {
-			printf("Did not find at level: %i\n", level);
-		}
-		printf("in loop\n");
+
 		i++;				// loop increment
 		level = (level + 1) % n;	// i points to next possible level
 	}
