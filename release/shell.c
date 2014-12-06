b5d8a898aeeb249317b1c884da92df8a11387e7c
Mark LaPointe <mnl38@cornell.edu>
2014-12-06 16:08:11 -0500

    Project 6: Replaced gets() with fgets()

--- release/shell.c
+++ release/shell.c
@@ -121,7 +121,7 @@ int inputfile(char *fname) {
 	printf("---inputting %s: input \"ctrl-D,ENTER\" on a new line to exit---\n",fname);
 	do {
 		memset(str,'\0',BUFFER_SIZE);
-		gets(str);
+		fgets(str);
 		if(str[0] == 4) //that's what ctrl-D puts in ((char)4)
 			break;
 		str[strlen(str)] = '\n';
